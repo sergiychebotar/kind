70c9965a06dcb73e3190304a646fdc77bc25438a
Serhii Chebotar <schebotar@teamworkretail.com>
2026-04-11 13:07:42 +0200

    terraform added

---
+++ terraform/monitoring-stack/main.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+# Prometheus stack (включає Grafana!)
+resource "helm_release" "kube_prometheus" {
+  name       = "kube-prometheus"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+
+  values = [
+    file("${path.module}/values/prometheus.yaml")
+  ]
+}
+
+# Loki
+resource "helm_release" "loki" {
+  name       = "loki"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-stack"
+
+  values = [
+    file("${path.module}/values/loki.yaml")
+  ]
+}
